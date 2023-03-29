2f3414c4e83b2df9b1c3abe4ab4be3132a95eebb
maheshwari <archit.maheshwari@payufin.com>
2023-03-29 09:42:24 +0530

    Tree questions, different traversals

---
+++ 63_BalancedBinaryTree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int solve(TreeNode *root){
+        if(!root) return 0;
+
+        int lh = solve(root->left);
+        int rh = solve(root->right);
+        if(lh==-1 || rh==-1) return -1;
+        if(abs(lh-rh)>1) return -1;
+        return 1 + max(solve(root->left), solve(root->right));
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return solve(root)!=-1;
+    }
+};
---
+++ 65_MaximumPathSum.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int solve(TreeNode *root, int &sum){
+        if(!root) return 0;
+
+        int ls = max(0, solve(root->left, sum));
+        int rs = max(0, solve(root->right, sum));
+
+        sum = max(sum, root->val + ls + rs);
+
+        return root->val + max(ls, rs);
+
+    }
+
+    int maxPathSum(TreeNode* root) {
+        int sum=INT_MIN;
+        solve(root, sum);
+        return sum;
+    }
+};
